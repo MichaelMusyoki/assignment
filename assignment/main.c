fee818fde291a7d122c6bc0fca5ec4a4aa0b8528
MichaelMusyoki <146918998+MichaelMusyoki@users.noreply.github.com>
2023-10-04 09:21:46 +0300

    Add files via upload

---
+++ assignment/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int a;
+    a=15;
+    printf("Hello world!\n");
+    printf("todays temprature is ");
+    printf("%d", a);
+    printf("degrees celcious");
+    return 0;
+}
